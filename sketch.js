10e1bc53f85c10f797bd0d7740a1c2204eb133fe
Gabi <pagameri@gmail.com>
2022-06-14 15:14:29 +0100

    Add option for user to set size
    Organised into functions so they can neatly call each other.
    User is now able to change size, left default at 16 boxes.

--- sketch.js
+++ sketch.js
@@ -1,28 +1,55 @@
 const grid = document.querySelector('.grid');
-const cells = document.getElementsByClassName('cell');
-const sizeBtn = document.querySelector('button');
-const side = 16;
 
-// console.log(cells);
+function changeColor() {
+  const cells = document.getElementsByClassName('cell');
+  Array.from(cells).forEach((cell) => {
+    cell.addEventListener('mouseenter', e => {
+      e.target.classList.add("hovered");
+    });
+  });
+}
 
-for (let row = 0; row < side; row++) {
-  const divRow = document.createElement('div');
-  divRow.innerHTML = "";
-  divRow.classList = "row";
-  grid.appendChild(divRow);
-  for (let column = 0; column < side; column++) {
-    const divColumn = document.createElement('div');
-    divColumn.classList = "cell";
-    divRow.appendChild(divColumn);
+function removeGrid(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
 }
 
-function colorBox(e) {
-  console.log(e.target);
-  // e.target.style.backgroundColor = "blue"; // Option 1
-  e.target.classList.add("hovered"); // Option 2
+function drawGrid(size) {
+  for (let row = 0; row < size; row++) {
+    const divRow = document.createElement('div');
+    divRow.innerHTML = "";
+    divRow.classList = "row";
+    grid.appendChild(divRow);
+    for (let column = 0; column < size; column++) {
+      const divColumn = document.createElement('div');
+      divColumn.classList = "cell";
+      divRow.appendChild(divColumn);
+    }
+  }
+  changeColor();
 }
 
-Array.from(cells).forEach((cell) => {
-  cell.addEventListener('mouseenter', colorBox);
-  });
+function getGridSize() {
+  sideLength = prompt("How many boxes should each side have? (between 1 and 100)", 16);
+ if (sideLength === null) {
+  return;
+} else if (sideLength < 1 || sideLength > 100) {
+    getGridSize();
+  }
+  removeGrid(grid);
+  drawGrid(sideLength);
+}
+
+
+function newGrid() {
+  const sizeBtn = document.querySelector('.size');
+  sizeBtn.addEventListener('click', getGridSize);
+}
+
+function playGame() {
+  drawGrid(16);
+  newGrid();
+}
+
+playGame();
