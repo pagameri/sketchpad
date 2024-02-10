55c7c223fa8dec37f3f20913794fd1873e6e7ddb
pagameri <pagameri@gmail.com>
2024-02-10 23:25:05 +0000

    Add colour on hover + random color

--- pad-ui.js
+++ pad-ui.js
@@ -3,5 +3,12 @@ const gameContainer = document.querySelector('#game-container');
 for (let i = 0; i < 256; i++) {
   const square = document.createElement('div');
   square.style.cssText = 'width: 20px; height: 20px; border: 1px solid black';
+  square.addEventListener('mouseenter', changeColor);
   gameContainer.appendChild(square);
+}
+
+function changeColor(e) {
+  // e.target.className = 'colored';
+  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  e.target.style.backgroundColor = '#' + randomColor;
 }
