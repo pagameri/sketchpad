4546da2e52d3b90e39e0164992d183a5adaad9d4
pagameri <pagameri@gmail.com>
2024-02-12 15:16:26 +0000

    Add grid size change option

--- pad-ui.js
+++ pad-ui.js
@@ -1,14 +1,44 @@
 const gameContainer = document.querySelector('#game-container');
 
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement('div');
-  square.style.cssText = 'width: 20px; height: 20px; border: 1px solid black';
-  square.addEventListener('mouseenter', changeColor);
-  gameContainer.appendChild(square);
+const sizeBtn = document.querySelector('#change-size');
+
+let gameBoardSize = 500;
+drawGrid(60);
+gameContainer.style.width = `${gameBoardSize}px`;
+
+sizeBtn.addEventListener('click', () => {
+  let gridSide;
+  do {
+    gridSide = prompt('How many grids per side? (Number between 2 and 100)');
+  } while (2 > gridSide || gridSide > 100);
+  clearGrid();
+  drawGrid(gridSide);
+});
+
+function clearGrid() {
+  gameContainer.innerHTML = '';
+}
+
+function drawGrid(grids) {
+  let totalGrids = grids * grids;
+  let boxSize = gameBoardSize / grids;
+  for (let i = 0; i < totalGrids; i++) {
+    const square = document.createElement('div');
+    square.className = 'square';
+    square.style.width = `${boxSize}px`;
+    square.style.height = `${boxSize}px`;
+    square.addEventListener('mouseover', changeColor);
+    gameContainer.appendChild(square);
+  }
 }
 
 function changeColor(e) {
   // e.target.className = 'colored';
-  let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  e.target.style.backgroundColor = '#' + randomColor;
+  // let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  // e.target.style.backgroundColor = '#' + randomColor;
+  // let rand = Math.floor(Math.random() * 256);
+  let randomRGB = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
+    Math.random() * 256
+  )}, ${Math.floor(Math.random() * 256)})`;
+  e.target.style.backgroundColor = randomRGB;
 }
